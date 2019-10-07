27d6b6aa9f0154eda69b1d1c8338946e1899a47c
SeYun-Kim <tpdbs72@cu.ac.kr>
2019-10-07 14:04:02 +0900

    add

---
+++ ch05/Q1.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+int factorial(int n)
+{
+	int i =0;
+	int result = 1;
+
+	for (i = n; i > 0; i--)
+	{
+		result *= i;
+	}
+	return result;
+}
+
+int main(void)
+{
+	int x;
+	printf("정수를 입력하세요：");
+	scanf("%d", &x);
+	printf("%d의 순차곱셈 값은 %d입니다.\n", x, factorial(x));
+
+	return 0;
+}
